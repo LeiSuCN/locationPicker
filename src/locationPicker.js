670ac5e8f1e95285aa5be17d21d894d1b71d1b42
LeiSuCN <s.lei1116@163.com>
2015-05-06 22:42:37 +0800

    add more configrations

--- src/locationPicker.js
+++ src/locationPicker.js
@@ -4,34 +4,44 @@
 
 		var _this = this;
 
-		var _bm = new BMap.Map(containerId);
-		_bm.centerAndZoom("深圳",12);
-
 		var _opts = {};
 		$.extend(_opts, {
 			pick:function(point){
 				console.log( point.lng + ', ' + point.lat );
-			}
+			},
+			enableClick: true,
+			enableSearch: true,
+			mouseCursor: 'crosshair',
+			center: "深圳",
+			zoom: 12
 		}, opts);
 
+
+		var _bm = new BMap.Map(containerId);
+		_bm.centerAndZoom(_opts.center,_opts.zoom);
+
 		// cursor style
-		_bm.setDefaultCursor('crosshair');
+		_bm.setDefaultCursor(_opts.mouseCursor);
 
 		// wheel zoom
 		_bm.enableScrollWheelZoom();
 		_bm.enableContinuousZoom();
 
 		// for the click event
-		_bm.addEventListener("click", function(e){
-			_this.pick(e.point);
-		});
+		if( _opts.enableClick ){
+			_bm.addEventListener("click", function(e){
+				_this.pick(e.point);
+			});
+		}		
 
 		// private members
 		this._bm = _bm;
 		this._opts = _opts;
 
 		// add autocomplete
-		this.addAutocomplete();
+		if( _opts.enableSearch ){
+			this.addAutocomplete();
+		}
 	}
 
 
@@ -97,11 +107,27 @@
 		this._bm.clearOverlays();
 		this._bm.addOverlay(new BMap.Marker(point)); 
 
-		this._bm.setCenter(point);
-
 		if( this._opts.pick )
 			this._opts.pick(point)
 		
+	};
+
+	LocationPicker.prototype.setCenter = function() {
+
+		if( arguments )
+		{
+			if( arguments.length == 1){ // setCenter(point)
+				this._bm.setCenter(arguments[0]);
+			}
+			else if( arguments.length == 2){ // setCenter(lng, lat)
+				var point = new BMap.Point(arguments[0], arguments[1]);
+				this._bm.setCenter(point);
+			}
+		}
+	};
+
+	LocationPicker.prototype.setZoom = function(zoom) {
+		this._bm.setZoom(zoom);
 	};
 
 
